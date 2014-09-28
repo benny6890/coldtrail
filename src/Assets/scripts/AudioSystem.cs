c0d398e04525d76d0def83a6dfd4e89e91242450
detlog <detlog@sbox.tugraz.at>
2014-09-28 12:00:47 +0200

    Sound System added

---
+++ src/Assets/scripts/AudioSystem.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSystem : MonoBehaviour {
+
+
+	AudioSource BGMAudio;
+	AudioSource SFX;
+
+	public AudioClip collectFood;
+	public AudioClip collectWood;
+	public AudioClip[] BGM;
+	public AudioClip step;
+
+	public float volumeBGM = 0.7f;
+	public float volumeSFX = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+
+		BGMAudio = this.gameObject.AddComponent<AudioSource> ();
+		SFX = this.gameObject.AddComponent<AudioSource> ();
+		BGMAudio.clip = BGM[0];
+		BGMAudio.volume = volumeBGM;
+		BGMAudio.loop = true;
+		SFX.volume = volumeSFX;
+
+		PlayBgm ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.N)) 
+			SwitchBGMMusic ();	
+	}
+
+	public void PlayBgm() {
+		BGMAudio.Play ();
+	}
+
+	public void StopBgm() {
+		BGMAudio.Stop ();
+	}
+
+	public void PlayCollectWood() {
+		SFX.clip = collectWood;
+		SFX.Play ();
+	}
+	public void PlayCollectFood() {
+		SFX.clip = collectFood;
+		SFX.Play ();
+	}
+
+	public void PlayStep() {
+		SFX.clip = step;
+		SFX.Play ();
+	}
+
+	public void SwitchBGMMusic() {
+		BGMAudio.Stop ();
+		BGMAudio.clip =BGM[Mathf.RoundToInt (Random.Range (1.0f, BGM.Length-1))];
+		BGMAudio.Play ();
+	}
+
+}
